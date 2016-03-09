adfe976051c341df5442f14364e80bd780ac7da9
Robert Veznaver <r.veznaver@criteo.com>
2016-03-09 18:16:27 +0100

    Release 0.2.2

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
 # Disable DataBag fallback in the chef-vault cookbook
 default['chef-vault']['databag_fallback'] = false
 
-# The version of chef-vault has to be the same as the bundled version
-default['chef-vault']['version'] = '2.7.1'
+# The version of chef-vault should be the same as the bundled version
+default['chef-vault']['version'] = '2.8.0'
--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'sre-core@criteo.com'
 license          'All rights reserved'
 description      'Installs/Configures chef-vault with helpers'
 long_description 'Installs/Configures chef-vault with helpers'
-version          '0.2.1'
+version          '0.2.2'
 
 depends 'chef-vault'
