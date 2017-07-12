e9701c6323afe2028e53b34f22d53dc54701589a
Grégoire Seux <g.seux@criteo.com>
2017-07-12 09:10:32 +0200

    Make metadata.rb compatible with old chef version
    Change-Id: I2096d97756822278ca1ba568e2b346b8bd09926c

--- metadata.rb
+++ metadata.rb
@@ -5,8 +5,8 @@ license          'All rights reserved'
 description      'Installs/Configures chef-vault with helpers'
 long_description 'Installs/Configures chef-vault with helpers'
 version          '0.5.0'
-source_url       'https://github.com/criteo-cookbooks/chef-secrets'
-issues_url       'https://github.com/criteo-cookbooks/chef-secrets/issues'
+source_url       'https://github.com/criteo-cookbooks/chef-secrets' if defined?(source_url)
+issues_url       'https://github.com/criteo-cookbooks/chef-secrets/issues' if defined?(issues_url)
 
 depends 'chef-vault', '~> 2.1' # cookbook chef-vault 3.0.0 breaks our behavior for now
 
