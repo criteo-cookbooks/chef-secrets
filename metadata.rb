f3f11f0c9396671f30b1182b215e5433031e5f01
Baptiste Courtois <b.courtois@criteo.com>
2020-10-13 12:22:04 +0200

    [Major] Bump chef-vault gem to 4.0.12 & drop support for Chef < 14.0
    Latest chef-vault version requires a more recent ruby, which is embeded
    with Chef-infra >= 14.0
    
    We decided to extend official Chef infra support a bit.
    Chef 14 is officially EOL since april 2020, we'll at list keep it
    supported here for 1 year.
    
    Also bump Chef version in kitchen and the ruby version in travis to
    have consistent tests.

--- metadata.rb
+++ metadata.rb
@@ -4,9 +4,11 @@ maintainer_email 'sre-core@criteo.com'
 license          'All rights reserved'
 description      'Installs/Configures chef-vault with helpers'
 long_description 'Installs/Configures chef-vault with helpers'
-version          '1.0.1'
+version          '2.0.0'
 source_url       'https://github.com/criteo-cookbooks/chef-secrets' if defined?(source_url)
 issues_url       'https://github.com/criteo-cookbooks/chef-secrets/issues' if defined?(issues_url)
 
+chef_version '>= 14.0' if respond_to?(:chef_version)
+
 depends 'chef-vault', '~> 2.1' # cookbook chef-vault 3.0.0 breaks our behavior for now
-gem 'chef-vault', '3.4.3'
+gem 'chef-vault', '4.0.12'
